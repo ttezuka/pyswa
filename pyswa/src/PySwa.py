e24cc9c08c91e0857ab46bc88dd6b80e2ccff8b2
ttezuka <Tomonori.Tezuka@gmail.com>
2009-05-29 23:04:28 +0900

    Define interface
    First commit.

---
+++ pyswa/src/PySwa.py
@@ -0,0 +1,17 @@
+'''
+Created on 2009/05/29
+
+@author: ttezuka
+'''
+
+class PySwa:
+    def getAttachments(self,requestString):
+        return ""
+
+def Main():
+    pyswa = PySwa()
+    print pyswa.getAttachments("")
+
+if __name__ == "__main__":
+    Main()
+
